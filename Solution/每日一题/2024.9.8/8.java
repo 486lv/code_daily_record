48c1bb70643a0db99aba75457d44bcd668ca8861
refine_lv <144577122+lvlebin2876587146@users.noreply.github.com>
2024-09-09 16:23:05 +0800

    Create 8.java

---
+++ Solution/每日一题/2024.9.8/8.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()){
+            if (Character.isDigit(c)) {
+                res.deleteCharAt(res.length() - 1);
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
+
