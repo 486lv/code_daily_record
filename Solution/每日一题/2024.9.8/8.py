9a4e20be960d816cf6f08bd8378f5d32717fddfc
refine_lv <144577122+lvlebin2876587146@users.noreply.github.com>
2024-09-09 16:23:24 +0800

    Create 8.py

---
+++ Solution/每日一题/2024.9.8/8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        res = []
+        for c in s:
+            if c.isdigit():
+                res.pop()
+            else:
+                res.append(c)
+        return ''.join(res)
+
